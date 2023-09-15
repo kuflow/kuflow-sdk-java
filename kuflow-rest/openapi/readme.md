c5bddaad128be41c6dd57f5daa07dcb31dde2cbe
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2023-09-15 08:29:15 +0100

    Use the correct openapi file

--- kuflow-rest/openapi/readme.md
+++ kuflow-rest/openapi/readme.md
@@ -27,7 +27,7 @@ java: true
 title: KuFlow
 override-client-name: KuFlowClient
 
-input-file: https://raw.githubusercontent.com/kuflow/kuflow-openapi/336b24183db579748802c9170286dece8d1a64fa/specs/api.kuflow.com/v2022-10-08/openapi.yaml
+input-file: https://raw.githubusercontent.com/kuflow/kuflow-openapi/bf7b04c24f66bf5283f89471198417e89e21916a/specs/api.kuflow.com/v2022-10-08/openapi.yaml
 output-folder: ../target/openapi-generated
 
 openapi-type: data-plane
--- kuflow-rest/src/generated/java/com/kuflow/rest/model/Authentication.java
+++ kuflow-rest/src/generated/java/com/kuflow/rest/model/Authentication.java
@@ -48,7 +48,8 @@ public final class Authentication extends AbstractAudited {
 
     /*
      * Engine authentication token.
-     * @deprecated use engineConfig.token
+     *
+     * @deprecated use engineToken.token
      *
      */
     @JsonProperty(value = "token")
@@ -56,7 +57,8 @@ public final class Authentication extends AbstractAudited {
 
     /*
      * Engine authentication token expiration.
-     * @deprecated use engineConfig.expiredAt
+     *
+     * @deprecated use engineToken.expiredAt
      *
      */
     @JsonProperty(value = "expiredAt")
@@ -120,7 +122,7 @@ public final class Authentication extends AbstractAudited {
     /**
      * Get the token property: Engine authentication token.
      *
-     * @deprecated use engineConfig.token.
+     * @deprecated use engineToken.token.
      * @return the token value.
      */
     public String getToken() {
@@ -130,7 +132,7 @@ public final class Authentication extends AbstractAudited {
     /**
      * Set the token property: Engine authentication token.
      *
-     * @deprecated use engineConfig.token.
+     * @deprecated use engineToken.token.
      * @param token the token value to set.
      * @return the Authentication object itself.
      */
@@ -142,7 +144,7 @@ public final class Authentication extends AbstractAudited {
     /**
      * Get the expiredAt property: Engine authentication token expiration.
      *
-     * @deprecated use engineConfig.expiredAt.
+     * @deprecated use engineToken.expiredAt.
      * @return the expiredAt value.
      */
     public OffsetDateTime getExpiredAt() {
@@ -152,7 +154,7 @@ public final class Authentication extends AbstractAudited {
     /**
      * Set the expiredAt property: Engine authentication token expiration.
      *
-     * @deprecated use engineConfig.expiredAt.
+     * @deprecated use engineToken.expiredAt.
      * @param expiredAt the expiredAt value to set.
      * @return the Authentication object itself.
      */
