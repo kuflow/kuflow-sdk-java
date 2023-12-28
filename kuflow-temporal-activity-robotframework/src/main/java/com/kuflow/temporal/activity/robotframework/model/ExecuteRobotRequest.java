812f65e27a34280ce9008ce2101be4bdb4c9eec8
Zebensui Méndez Corujo <zmencor@kuflow.com>
2023-12-28 07:55:57 +0000

    Javadoc

--- kuflow-temporal-activity-robotframework/src/main/java/com/kuflow/temporal/activity/robotframework/model/ExecuteRobotRequest.java
+++ kuflow-temporal-activity-robotframework/src/main/java/com/kuflow/temporal/activity/robotframework/model/ExecuteRobotRequest.java
@@ -22,6 +22,9 @@
  */
 package com.kuflow.temporal.activity.robotframework.model;
 
+import static java.util.Collections.unmodifiableList;
+import static java.util.Collections.unmodifiableMap;
+
 import com.kuflow.temporal.common.model.AbstractModel;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -30,33 +33,81 @@ import java.util.Map;
 
 public class ExecuteRobotRequest extends AbstractModel {
 
+    /**
+     * Paths to test case files/directories to be executed similarly
+     * as when running the <code>robot</code> command on the command line.
+     * If the <code>KUFLOW_ROBOT_PATH</code> environment variable defined in the
+     * <b>python activity worker</b> exists, Its content is added as a prefix to the value
+     * of this field.
+     * @see <a href="https://github.com/kuflow/kuflow-sdk-python/blob/main/kuflow-temporal-activity-robotframework/kuflow_temporal_activity_robotframework/robot_framework_activities.py">KuFlow Python Robot Framework</a>
+     */
     private String tests;
 
+    /**
+     * Options to configure and control execution.
+     * Take precedence over a possible <code>variable</code> key present in
+     * {@link ExecuteRobotRequest#getOptions() options}.
+     * Take precedence over <code>default_options</code> in python class <code>RobotFrameworkActivities</code>.
+     * Values are merged.
+     * @see <a href="https://github.com/kuflow/kuflow-sdk-python/blob/main/kuflow-temporal-activity-robotframework/kuflow_temporal_activity_robotframework/robot_framework_activities.py">KuFlow Python Robot Framework</a>
+     * @see <a href="https://robot-framework.readthedocs.io/en/latest/_modules/robot/run.html">Robot Framework: robot.run</a>
+     */
     private List<Object> variables = new LinkedList<>();
 
+    /**
+     * Options to configure and control execution.
+     * Take precedence over <code>default_options</code> in python class <code>RobotFrameworkActivities</code>.
+     * Values are merged.
+     * @see <a href="https://github.com/kuflow/kuflow-sdk-python/blob/main/kuflow-temporal-activity-robotframework/kuflow_temporal_activity_robotframework/robot_framework_activities.py">KuFlow Python Robot Framework</a>
+     * @see <a href="https://robot-framework.readthedocs.io/en/latest/_modules/robot/run.html">Robot Framework: robot.run</a>
+     */
     private Map<String, Object> options = new HashMap<>();
 
+    /**
+     * See {@link ExecuteRobotRequest#tests}
+     */
     public String getTests() {
         return this.tests;
     }
 
+    /**
+     * @param tests See {@link ExecuteRobotRequest#tests}
+     */
     public void setTests(String tests) {
         this.tests = tests;
     }
 
+    /**
+     * See {@link ExecuteRobotRequest#variables}
+     */
     public List<Object> getVariables() {
-        return this.variables;
+        return unmodifiableList(this.variables);
     }
 
+    /**
+     * @param tests See {@link ExecuteRobotRequest#variables}
+     */
     public void setVariables(List<Object> variables) {
-        this.variables = variables;
+        this.variables.clear();
+        if (this.variables != null) {
+            this.variables.addAll(variables);
+        }
     }
 
+    /**
+     * See {@link ExecuteRobotRequest#options}
+     */
     public Map<String, Object> getOptions() {
-        return this.options;
+        return unmodifiableMap(this.options);
     }
 
+    /**
+     * @param tests See {@link ExecuteRobotRequest#options}
+     */
     public void setOptions(Map<String, Object> options) {
-        this.options = options;
+        this.options.clear();
+        if (this.options != null) {
+            this.options.putAll(options);
+        }
     }
 }
