1abd979f5ef42ad6158bc574dc00866ef5fc6084
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2024-06-03 14:00:45 +0100

    Add S3 activity "copyTaskJsonFormsFiles"

---
+++ kuflow-temporal-activity-s3/src/main/java/com/kuflow/temporal/activity/s3/model/CopyJsonFormsFile.java
@@ -0,0 +1,95 @@
+/*
+ * The MIT License
+ * Copyright © 2021-present KuFlow S.L.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.kuflow.temporal.activity.s3.model;
+
+/**
+ * File details copied.
+ */
+public class CopyJsonFormsFile {
+
+    private String propertyPath;
+
+    /**
+     * Bucket where the file was copied
+     */
+    private String bucket;
+
+    /**
+     * S3 Key where the file was copied
+     */
+    private String key;
+
+    /**
+     * The json forms property path
+     *
+     * @return the propertyPath
+     */
+    public String getPropertyPath() {
+        return this.propertyPath;
+    }
+
+    /**
+     * @see #getPropertyPath()
+     *
+     * @param propertyPath the json forms property path
+     */
+    public void setPropertyPath(String propertyPath) {
+        this.propertyPath = propertyPath;
+    }
+
+    /**
+     * Bucket where the file was copied
+     *
+     * @return the bucket name
+     */
+    public String getBucket() {
+        return this.bucket;
+    }
+
+    /**
+     * @see #getBucket()
+     *
+     * @param bucket the bucket
+     */
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    /**
+     * S3 Key where the file was copied
+     *
+     * @return S3 key
+     */
+    public String getKey() {
+        return this.key;
+    }
+
+    /**
+     * @see #getKey()
+     *
+     * @param key the s3 key
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
