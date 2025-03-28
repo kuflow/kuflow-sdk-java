44f08e775f8300b36cdcf1832c12f8e8fb06e7da
Zebensui Méndez Corujo <zmencor@kuflow.com>
2025-03-28 14:08:27 +0000

    Add user action signal contract (#117)
    * Add user action signal contract
    
    * Requested changes

---
+++ kuflow-temporal-workflow-kuflow/src/main/java/com/kuflow/temporal/workflow/kuflow/model/SignalUserAction.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021-present KuFlow S.L.
+ *
+ * All rights reserved.
+ */
+
+package com.kuflow.temporal.workflow.kuflow.model;
+
+public class SignalUserAction {
+
+    private String userActionDefinitionCode;
+
+    public String getUserActionDefinitionCode() {
+        return this.userActionDefinitionCode;
+    }
+
+    public void setUserActionDefinitionCode(String userActionDefinitionCode) {
+        this.userActionDefinitionCode = userActionDefinitionCode;
+    }
+}
