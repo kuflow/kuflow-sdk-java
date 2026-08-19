9191bcd1a90a1bede003bfed85bd23c820212a77
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2026-08-19 13:48:29 +0100

    refactor(temporal): remove unused invocation-related fields from DataSourceActivityRequest (#173)
    * refactor(temporal): remove unused invocation-related fields from DataSourceActivityRequest
    
    * refactor(temporal): remove unused Duration import from DataSourceActivityRequest

--- kuflow-temporal-activity-datasource/src/main/java/com/kuflow/temporal/activity/datasource/model/DataSourceActivityRequest.java
+++ kuflow-temporal-activity-datasource/src/main/java/com/kuflow/temporal/activity/datasource/model/DataSourceActivityRequest.java
@@ -25,7 +25,6 @@ package com.kuflow.temporal.activity.datasource.model;
 
 import com.kuflow.common.Experimental;
 import jakarta.validation.constraints.NotNull;
-import java.time.Duration;
 import java.util.UUID;
 
 /**
@@ -41,12 +40,6 @@ public abstract class DataSourceActivityRequest {
     @NotNull
     private String code;
 
-    private String invocationActivityName;
-
-    private Duration invocationTimeout;
-
-    private String invocationTaskQueue;
-
     public UUID getTenantId() {
         return this.tenantId;
     }
@@ -61,29 +54,5 @@ public abstract class DataSourceActivityRequest {
 
     public void setCode(String code) {
         this.code = code;
-    }
-
-    public String getInvocationActivityName() {
-        return this.invocationActivityName;
-    }
-
-    public void setInvocationActivityName(String invocationActivityName) {
-        this.invocationActivityName = invocationActivityName;
-    }
-
-    public Duration getInvocationTimeout() {
-        return this.invocationTimeout;
-    }
-
-    public void setInvocationTimeout(Duration invocationTimeout) {
-        this.invocationTimeout = invocationTimeout;
-    }
-
-    public String getInvocationTaskQueue() {
-        return this.invocationTaskQueue;
-    }
-
-    public void setInvocationTaskQueue(String invocationTaskQueue) {
-        this.invocationTaskQueue = invocationTaskQueue;
     }
 }
